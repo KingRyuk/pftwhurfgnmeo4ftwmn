01d513320ca8b71c6b5ab719c517ac13800e9ff0
KingRyuk <shotoisapug@gmail.com>
2025-06-21 14:32:10 +0100

    fgdj

--- _site/zgame/Warriorcatsplanner/src/components/CopyTextButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function CopyTextButton({ label = "📋 Copy Text", onCopy }) {
-  return (
-    <button className="button copy-text-btn" onClick={onCopy}>
-      {label}
-    </button>
-  );
-}
